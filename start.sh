b77de38be92635687030f791a98f7ec40ca6ea9a
jenghung <jenghung.chen@gmail.com>
2017-04-12 09:09:21 +0800

    modify command.

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 iptables -t nat -A OUTPUT -p tcp -d 192.168.30.0/24 -j REDIRECT --to-ports 12345
 iptables -t nat -A OUTPUT -p tcp -d 192.168.31.0/24 -j REDIRECT --to-ports 12345
 iptables -t nat -A OUTPUT -p tcp -d 192.168.32.0/24 -j REDIRECT --to-ports 12345
+./redsocks &
--- stop.sh
+++ stop.sh
@@ -2,3 +2,4 @@
 iptables -t nat -D OUTPUT 3
 iptables -t nat -D OUTPUT 2
 iptables -t nat -D OUTPUT 1
+pkill -f redsocks
